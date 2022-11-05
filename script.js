e2cbf20b6a2d2e21cb7d0ffd47da7bca06769cc0
andreydeshko <andydeshko@gmail.com>
2022-11-05 13:03:28 +0200

    init commit

---
+++ script.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const acc1 = {
+    supplierName: 'A',
+    supplierPass: 123,
+}
+
+const acc2 = {
+    supplierName: 'B',
+    supplierPass: 234,
+}
+
+
+const accounts = [acc1, acc2];
+
+const username = document.getElementById('username').value;
+const password = document.getElementById('password').value;
+const btnLogin = document.querySelector('btnLogin');
+
+btnLogin.addEventListener('click', function(event) {
+    
+})
+
+
